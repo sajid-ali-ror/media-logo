f911f025a6a3b7f55eb3a69153ab09860cda0df4
Sajidali <sajidviralsquare@gmail.com>
2021-10-20 21:13:38 +0500

    Feed back issue fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,19 +15,21 @@ ActiveStorage.start()
 $(document).ready(function () {
     $(".gray_inactive").hide()
     $(".card_text_area").text($(".gray_active").html())
-    $(".input_field").on(" focusout", function () {
-        debugger
+    $(document).on("focusout",".input_field", function () {
         var id = this.id
         if (this.value.length > 4) {
             $(".card_text_area").text()
            var domain =  this.value.split("www.")
-            $("." + id).attr('href', "https://www."+domain);
-            $("." + id + "_img").attr('src', "https://logo.clearbit.com/" + domain + "?size=40");
-            $("." + id + "_img_gray").attr('src', "https://logo.clearbit.com/" + domain + "?size=40&greyscale=true");
+            if (domain.length > 1){
+                domain =  domain[1].split(".com/")[0]+".com"
+            }
+            $("#" + id+ "_tag").attr('href', "https://www."+domain);
+            $("#" + id + "_img" ).attr('src', "https://logo.clearbit.com/" + domain + "?size=40");
+            $("#" + id+ "_img_gray" ).attr('src', "https://logo.clearbit.com/" + domain + "?size=40&greyscale=true");
             $(".card_text_area").text($(".gray_active").html())
         }
     })
-    $(".grayscale").on("click", function () {
+    $(document).on("click",".grayscale", function () {
         $(".card_text_area").text()
         if (this.checked) {
             $(".gray_inactive ").show()
@@ -38,5 +40,15 @@ $(document).ready(function () {
             $(".gray_active").show()
             $(".card_text_area").text($(".gray_active").html())
         }
+    })
+    $(document).on("click",".button4", function () {
+        var count = $(".input_field").length
+        $(".input_dev").append(`<input class='form-control ng-pristine ng-valid ng-empty ng-touched input_field' id="input_field_${count}" placeholder='Link to Paste Here' type='text'>\n`)
+        $(".gray_active").append(`<a class='prj-logo grayscale-true abcLink' href='' id="input_field_${count}_tag"  target='_blank'>\n`+
+            `<img class='abcLink_img'  id="input_field_${count}_img" src='https://pressreleasejet.com/assets/media-logos/logo-abc.png' style='height: 40px'>\n` +
+            `</a>`)
+        $(".gray_inactive").append(`<a class='prj-logo grayscale-false abcLink prj-grayscale-true' id="input_field_${count}_tag_gray" href='' target='_blank'>\n` +
+            `<img class='abcLink_img_gray' id="input_field_${count}_img_gray"  src='https://pressreleasejet.com/assets/media-logos/logo-abc.png' style='height: 40px'>\n` +
+            `</a>\n`)
     })
 });
